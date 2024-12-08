3d780f75ddf20511ffd4bd3e5e487652e672b8b3
scrityos <pixel.scrityos@gmail.com>
2024-12-08 11:40:25 +0530

    Update ExperimentalSettings.kt

--- app/src/main/java/com/dd3boh/outertune/ui/screens/settings/ExperimentalSettings.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/ExperimentalSettings.kt
@@ -1,161 +1,102 @@
 package com.dd3boh.outertune.ui.screens.settings
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Backup
+import androidx.compose.material.icons.rounded.DeveloperMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.dd3boh.outertune.BuildConfig
+import com.dd3boh.outertune.LocalDatabase
 import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
 import com.dd3boh.outertune.R
+import com.dd3boh.outertune.constants.DevSettingsKey
 import com.dd3boh.outertune.ui.component.IconButton
+import com.dd3boh.outertune.ui.component.PreferenceEntry
+import com.dd3boh.outertune.ui.component.PreferenceGroupTitle
+import com.dd3boh.outertune.ui.component.SwitchPreference
 import com.dd3boh.outertune.ui.utils.backToMain
+import com.dd3boh.outertune.utils.rememberPreference
+import com.dd3boh.outertune.utils.scanners.LocalMediaScanner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen(
+fun ExperimentalSettings(
     navController: NavController,
     scrollBehavior: TopAppBarScrollBehavior,
 ) {
-    val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
+    val database = LocalDatabase.current
+    val coroutineScope = rememberCoroutineScope()
+
+    // state variables and such
+    val (devSettings, onDevSettingsChange) = rememberPreference(DevSettingsKey, defaultValue = false)
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
+        Modifier
             .windowInsetsPadding(LocalPlayerAwareWindowInsets.current)
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .verticalScroll(rememberScrollState())
     ) {
-        Spacer(Modifier.height(4.dp))
 
-        Image(
-            painter = painterResource(R.drawable.launcher_monochrome),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground, BlendMode.SrcIn),
-            modifier = Modifier
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.surfaceColorAtElevation(NavigationBarDefaults.Elevation))
-                .clickable { }
+        // dev settings
+        SwitchPreference(
+            title = { Text(stringResource(R.string.dev_settings_title)) },
+            description = stringResource(R.string.dev_settings_description),
+            icon = { Icon(Icons.Rounded.DeveloperMode, null) },
+            checked = devSettings,
+            onCheckedChange = onDevSettingsChange
         )
 
-        Row(
-            verticalAlignment = Alignment.Top,
-        ) {
-            Text(
-                text = "OuterTune",
-                style = MaterialTheme.typography.headlineMedium,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 8.dp, bottom = 4.dp)
+        if (devSettings) {
+            PreferenceGroupTitle(
+                title = stringResource(R.string.settings_debug)
+            )
+            PreferenceEntry(
+                title = { Text("DEBUG: Nuke local lib") },
+                icon = { Icon(Icons.Rounded.Backup, null) },
+                onClick = {
+                    Toast.makeText(context, "Nuking local files from database...", Toast.LENGTH_SHORT).show()
+                    coroutineScope.launch(Dispatchers.IO) {
+                        Timber.tag("Settings").d("Nuke database status:  ${database.nukeLocalData()}")
+                    }
+                }
+            )
+            PreferenceEntry(
+                title = { Text("DEBUG: Force local to remote artist migration NOW") },
+                icon = { Icon(Icons.Rounded.Backup, null) },
+                onClick = {
+                    Toast.makeText(context, "Starting migration...", Toast.LENGTH_SHORT).show()
+                    coroutineScope.launch(Dispatchers.IO) {
+                        val scanner = LocalMediaScanner.getScanner()
+                        Timber.tag("Settings").d("Force Migrating local artists to YTM (MANUAL TRIGGERED)")
+                        scanner.localToRemoteArtist(database)
+                    }
+                }
             )
         }
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE}) | ${BuildConfig.FLAVOR}",
-                style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.secondary
-            )
-
-            Spacer(Modifier.width(4.dp))
-
-            if (BuildConfig.DEBUG) {
-                Spacer(Modifier.width(4.dp))
-
-                Text(
-                    text = "DEBUG",
-                    style = MaterialTheme.typography.labelSmall,
-                    color = MaterialTheme.colorScheme.secondary,
-                    modifier = Modifier
-                        .border(
-                            width = 1.dp,
-                            color = MaterialTheme.colorScheme.secondary,
-                            shape = CircleShape
-                        )
-                        .padding(
-                            horizontal = 6.dp,
-                            vertical = 2.dp
-                        )
-                )
-            }
-        }
-
-        Spacer(Modifier.height(4.dp))
-
-        Text(
-            text = "By Davide Garberi & Michael Zh.",
-            style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.secondary
-        )
-
-        Spacer(Modifier.height(8.dp))
-
-        Row {
-            IconButton(
-                onClick = { uriHandler.openUri("https://github.com/DD3Boh/OuterTune") }
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.github),
-                    contentDescription = null
-                )
-            }
-        }
-
-        Spacer(Modifier.height(8.dp))
-
-        Row(
-            verticalAlignment = Alignment.Top,
-        ) {
-            Text(
-                text = "Special Thanks",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 8.dp, bottom = 4.dp)
-            )
-        }
-
-        Text(
-            text = "Zion Huang for InnerTune",
-            style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.secondary
-        )
-
     }
 
+
+
+
     TopAppBar(
-        title = { Text(stringResource(R.string.about)) },
+        title = { Text(stringResource(R.string.experimental_settings_title)) },
         navigationIcon = {
             IconButton(
                 onClick = navController::navigateUp,
